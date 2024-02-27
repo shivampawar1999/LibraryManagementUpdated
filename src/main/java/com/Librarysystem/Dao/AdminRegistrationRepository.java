215d138c5c1b3566efa19b4e9fcfe08ef14e857d
Shivam Pawar <66766555+shivampawar1999@users.noreply.github.com>
2024-02-27 13:29:22 +0530

    Commenting AdmindDao List import -> forcefully get error
    Jenkins Build fail bug example

--- src/main/java/com/Librarysystem/Dao/AdminRegistrationRepository.java
+++ src/main/java/com/Librarysystem/Dao/AdminRegistrationRepository.java
@@ -1,6 +1,6 @@
 package com.Librarysystem.Dao;
 
-import java.util.List;
+//import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
